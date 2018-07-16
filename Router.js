7fb6dc26729e7cac82f1bcef2a4bf1931002a154
Christian Onuogu <conuogu@Christian-Onuogu-MacBook-Pro.local>
2018-07-15 20:57:18 -0700

    change to createBottomTabNavigator

--- Router.js
+++ Router.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Dimensions, Platform } from 'react-native';
-import { StackNavigator, TabNavigator } from 'react-navigation';
+import { StackNavigator, createBottomTabNavigator } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 
 import Appointments from './Appointments';
@@ -9,26 +9,26 @@ import Notes from './Notes';
 
 let screen = Dimensions.get('window');
 
-export const Tabs = TabNavigator({
+export const Tabs = createBottomTabNavigator({
   'Appointments': {
     screen: Appointments,
     navigationOptions: {
       tabBarLabel: 'Appointments',
-      tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />
+      //tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />
     },
   },
   'Chat': {
     screen: Chat,
     navigationOptions: {
       tabBarLabel: 'Chat',
-      tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />
+      //tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />
     },
   },
   'Notes': {
     screen: Notes,
     navigationOptions: {
       tabBarLabel: 'Notes',
-      tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />
+      //tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />
     },
   },
 });
