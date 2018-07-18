ebc54d838d2168e029b17633c2dbecbf4701b4e9
Christian Onuogu <conuogu@Christian-Onuogu-MacBook-Pro.local>
2018-07-17 21:45:45 -0700

    Worked on Chat and Notes page

--- Chat.js
+++ Chat.js
@@ -1,17 +1,55 @@
 import React, { Component } from 'react';
 import {
+  StatusBar,
   StyleSheet,
+  FlatList,
   Text,
   View
 } from 'react-native';
+import { Icon, Header } from 'react-native-elements';
+
+import ChatItem from './ChatItem';
 
 export default class Chat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [
+        {
+          id: 1,
+          advisor: 'Jenny Test',
+          lastMessage: 'I think you are on the right track...',
+          thumbnail: 'https://images.pexels.com/photos/355164/pexels-photo-355164.jpeg?auto=compress&cs=tinysrgb&h=350'
+        }
+      ]
+    }
+  }
+
+  _renderItem = ({item}) => (
+    <ChatItem
+      id={item.id}
+      advisor={item.advisor}
+      lastMessage={item.lastMessage}
+      thumbnail={item.thumbnail}
+    />
+  );
+
+  _keyExtractor = (item, index) => item.id.toString();
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>
-          Chat
-        </Text>
+        <Header
+          statusBarProps={{ barStyle: 'light-content' }}
+          leftComponent={{ icon: 'person', color: '#fff' }}
+          centerComponent={{ text: 'Chat', style: { color: '#fff' } }}
+          rightComponent={{ icon: 'add', color: '#fff' }}
+        />
+        <FlatList
+          data={this.state.books}
+          keyExtractor={this._keyExtractor}
+          renderItem={this._renderItem}
+        />
       </View>
     );
   }
@@ -20,13 +58,6 @@ export default class Chat extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  title: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
   }
 });
--- Notes.js
+++ Notes.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
 import {
+  StatusBar,
   StyleSheet,
+  FlatList,
   Text,
   View
 } from 'react-native';
+import { Icon, Header } from 'react-native-elements';
+
+import AppointmentItem from './AppointmentItem';
 
 export default class Notes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [
+        {
+          id: 1,
+          advisor: 'John Stockton',
+          date: '10/15 @ 3pm',
+          thumbnail: 'https://images.pexels.com/photos/542282/pexels-photo-542282.jpeg?auto=compress&cs=tinysrgb&h=350'
+        }
+      ]
+    }
+  }
+
+  _renderItem = ({item}) => (
+    <AppointmentItem
+      id={item.id}
+      advisor={item.advisor}
+      date={item.date}
+      thumbnail={item.thumbnail}
+    />
+  );
+
+  _keyExtractor = (item, index) => item.id.toString();
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>
-          Notes
-        </Text>
+        <Header
+          statusBarProps={{ barStyle: 'light-content' }}
+          leftComponent={{ icon: 'person', color: '#fff' }}
+          centerComponent={{ text: 'Notes', style: { color: '#fff' } }}
+        />
+        <FlatList
+          data={this.state.books}
+          keyExtractor={this._keyExtractor}
+          renderItem={this._renderItem}
+        />
       </View>
     );
   }
@@ -20,13 +57,6 @@ export default class Notes extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  title: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
   }
 });
